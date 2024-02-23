e2617a67199b4652687c59a496ac0d0483bbdb83
Ilyas-Shahi <ilyasm.shahi359@gmail.com>
2024-02-23 05:17:17 +0500

    chat db store

--- server/index.js
+++ server/index.js
@@ -7,46 +7,66 @@ import { Server } from 'socket.io';
 
 import userRouter from './routes/user.js';
 import authRouter from './routes/auth.js';
+import { Chat } from './models/Chat.js';
 
+// Configuration
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
-
+// Creating server instance for socket.io server constructor and allow cors for client origin
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: { origin: 'http://localhost:5173' },
+  cors: { origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173' },
 });
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Hello world</h1>');
-// });
-
+// API routes for auth and user
 app.use('/user', userRouter);
 app.use('/auth', authRouter);
 
+// WebSocket initialization
 io.on('connection', (socket) => {
   console.log('socket connected', socket.id);
   let currentRoom;
+  let currentChat;
 
-  // console.log(socket.handshake.auth);
+  // console.log(socket.handshake);
 
-  socket.on('start-chat', (room) => {
+  socket.on('start-chat', async (room) => {
     console.log('started chat on room', room);
 
     if (currentRoom) socket.leave(currentRoom);
 
     currentRoom = room;
     socket.join(room);
-    console.log(socket.rooms);
+
+    const chatData = await Chat.findOne({ room });
+    if (chatData === null) {
+      currentChat = await Chat.create({ room });
+    } else {
+      currentChat = chatData;
+    }
+    // console.log(socket.rooms);
   });
 
+  // if (chat) {
+
+  // }
+
   socket.on('send', (data) => {
-    console.log(data);
+    console.log('message send', data, currentRoom);
     socket.to(currentRoom).emit('receive', data);
+    // storeMessage(data, currentRoom);
+
+    currentChat.messages = [...currentChat.messages, { message: data }];
+
+    currentChat.save();
+
+    console.log(currentChat);
   });
 });
 
+// Server startup
 const port = process.env.PORT || 3000;
 httpServer.listen(port, () => {
   console.log('sever running on port ' + port);
--- server/models/Chat.js
+++ server/models/Chat.js
@@ -2,18 +2,24 @@ import mongoose, { Schema } from 'mongoose';
 
 const MessageSchema = new Schema(
   {
-    sender: { type: String, required: true },
-    receiver: { type: String, required: true },
-    message: { type: String, required: true },
+    sender: {
+      type: String,
+      // required: true
+    },
+    receiver: {
+      type: String,
+      //  required: true
+    },
+    message: { type: String || Number, required: true },
   },
   { timestamps: true }
 );
-export const Message = mongoose.model('Chat', MessageSchema);
+export const Message = mongoose.model('Message', MessageSchema);
 
 const ChatSchema = new Schema(
   {
     room: { type: String, required: true },
-    messages: [Message],
+    messages: [MessageSchema],
   },
   { timestamps: true }
 );
